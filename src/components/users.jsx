c3acba182c8eea4d52e9843356d767aa5aa88778
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-03-15 18:29:09 +0300

    render Qualities

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,36 +2,82 @@ import React, { useState } from 'react';
 import api from '../api'
 
 
+
+
 const Users = () => {
-    const [users, setUsers] = useState(api.users.fetchAll())
-    console.log(users)
-    const renderBadgeQualities = () => {
-            users.map((qualities) => {
-                return qualities.qualities.map((names) => {
-                    console.log(names)
-                }
-                    //  (
-                    //     <span className='badge bg-primary'>{names.name}</span>
+    const [ users, setUsers ] = useState(api.users.fetchAll())
+    // console.log(users)
+
+    // users.map((user) => {
+    //     console.log(...user.qualities)
+    // })
+
+    const renderBadgeQualities = ([{ color, name, _id }]) => {
+    let colorBadge =`badge bg-${color}`
+        return (
+                    <span key={_id} className={colorBadge}>{name}</span>
+                )
+
+        }
+// const Qualities = () => {
+//
+//           return   users.forEach((user) => {
+//              for (const quality of user.qualities) {
+//                  console.log(renderBadgeQualities(quality))
+//              }
+//
+//          } )
+//
+// }
+
+
+
+
+
+      // return users.forEach((user) =>
+      //       user.qualities.map((quality) =>
+      //       {
+      //           console.log(quality.name)
+      //       }
+                // (
+                //     <span className='badge bg-primary'>{quality.name}</span>
+                // )
+        //     )
+        // )
+
+
+    // return  users.map((user) =>
+            // user.qualities.map((quality) =>
+
+                    // console.log(user.qualities)
+
+                    // (
+                    //     <span className='badge bg-primary'>{user.qualities[0].name}</span>
                     // )
-                );
-            })
-    }
+
+    //         // )
+    // )
+    // }
+
+
 
     const renderUsers = () => {
-        // renderBadgeQualities()
+
         return users.map((user) => (
+
             <tr key={user._id}>
-                <td scope="row" >{user.name}</td>
-                <td className='badge bg-primary'>{renderBadgeQualities()}</td>
+                <td scope="row">{user.name}</td>
+                <td>{renderBadgeQualities(user.qualities)}</td>
                 <td>{user.profession.name}</td>
                 <td>{user.completedMeetings}</td>
                 <td>{user.rate}</td>
+                <td><button className='btn btn-primary btn-sm m-2'>Delete</button></td>
             </tr>
         ))
     }
     return (
         <>
-        <h1>Users</h1>
+            <h1>Users</h1>
             <table className="table">
                 <thead>
                 <tr>
@@ -43,7 +89,7 @@ const Users = () => {
                 </tr>
                 </thead>
                 <tbody>
-                    {renderUsers()}
+                {renderUsers()}
                 </tbody>
             </table>
         </>
