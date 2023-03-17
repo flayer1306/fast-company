bf3c02f59945fe191aebecf4e5851ddd4c26f61d
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-03-17 23:29:20 +0300

    add phrase, hadleDelete, quality

---
+++ src/components/phrase.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const phrase = (number) => {
+    const strNumber = String(number);
+    let phrase = '';
+    let colorBadge = '';
+    if (strNumber === '0') {
+        colorBadge = 'badge bg-danger';
+        phrase = 'Никто с тобой не тусанет сегодня';
+    } else if ((strNumber[strNumber.length-1] === '2' || strNumber[strNumber.length-1] === '3' || strNumber[strNumber.length-1] === '4')
+        && strNumber[strNumber.length-2] !== '1' ) {
+        colorBadge = 'badge bg-primary';
+        phrase = number+' человека тусанут с тобой сегодня';
+    } else {
+        colorBadge = 'badge bg-primary';
+        phrase = number+' человек тусанет с тобой сегодня';
+    }
+    return <span className={colorBadge}>{phrase}</span>;
+};
+
+export default phrase;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,83 +1,19 @@
 import React, { useState } from 'react';
 import api from '../api'
-
-
+import user from './user';
+import phrase from './phrase';
 
 
 const Users = () => {
-    const [ users, setUsers ] = useState(api.users.fetchAll())
-    // console.log(users)
+    const [ users, setUsers ] = useState(api.users.fetchAll());
 
-    // users.map((user) => {
-    //     console.log(...user.qualities)
-    // })
+    const handleDelete = (userId) => {
+        setUsers((prevState) => prevState.filter(item => item._id !== userId))
+    };
 
-    const renderBadgeQualities = ([{ color, name, _id }]) => {
-    let colorBadge =`badge bg-${color}`
-        return (
-                    <span key={_id} className={colorBadge}>{name}</span>
-                )
-
-        }
-// const Qualities = () => {
-//
-//           return   users.forEach((user) => {
-//              for (const quality of user.qualities) {
-//                  console.log(renderBadgeQualities(quality))
-//              }
-//
-//          } )
-//
-// }
-
-
-
-
-
-      // return users.forEach((user) =>
-      //       user.qualities.map((quality) =>
-      //       {
-      //           console.log(quality.name)
-      //       }
-                // (
-                //     <span className='badge bg-primary'>{quality.name}</span>
-                // )
-        //     )
-        // )
-
-
-    // return  users.map((user) =>
-            // user.qualities.map((quality) =>
-
-                    // console.log(user.qualities)
-
-                    // (
-                    //     <span className='badge bg-primary'>{user.qualities[0].name}</span>
-                    // )
-
-    //         // )
-    // )
-    // }
-
-
-
-    const renderUsers = () => {
-
-        return users.map((user) => (
-
-            <tr key={user._id}>
-                <td scope="row">{user.name}</td>
-                <td>{renderBadgeQualities(user.qualities)}</td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate}</td>
-                <td><button className='btn btn-primary btn-sm m-2'>Delete</button></td>
-            </tr>
-        ))
-    }
     return (
         <>
-            <h1>Users</h1>
+            <h1>{phrase(users.length)}</h1>
             <table className="table">
                 <thead>
                 <tr>
@@ -89,11 +25,11 @@ const Users = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {renderUsers()}
+                {user(users, handleDelete)}
                 </tbody>
             </table>
         </>
     )
-}
+};
 
 export default Users;
