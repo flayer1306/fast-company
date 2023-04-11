5392bb7f96fdaf555471e84f6a72238f7329f40d
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-04-12 01:10:10 +0300

    add IconSorted

--- src/app/components/tableHeader.jsx
+++ src/app/components/tableHeader.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TableHeader = ({ onSort, selectedSort, columns }) => {
+    const iconStyle = selectedSort.order === 'asc' ? 'bi bi-caret-up-fill' : 'bi bi-caret-down-fill';
+    const renderIconSort = (selectedSort, currentPath) => {
+        if (selectedSort.path === currentPath) {
+            return <i className={iconStyle}></i>;
+        }
+    };
     const handleSort = (item) => {
         if (selectedSort.path === item) {
             onSort({ ...selectedSort, order: selectedSort.order === 'asc' ? 'desc' : 'asc' });
@@ -15,12 +21,13 @@ const TableHeader = ({ onSort, selectedSort, columns }) => {
                 {Object.keys(columns).map((column) => (
                     <th key={column}
                         onClick = {columns[column].path
-                            ? () => handleSort(columns[column].path)
+                            ? () => handleSort(columns[column].path, column)
                             : undefined }
                         {...{ role: columns[column].path && 'button' }}
                         scope="col"
                     >
                         {columns[column].name}
+                        {renderIconSort(selectedSort, columns[column].path)}
                     </th>
                 ))}
             </tr>
